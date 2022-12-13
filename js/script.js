6f192c73aeb12402fc9557f8d75935063341dff0
EvaXausa <xausa.eva@gmail.com>
2022-12-13 16:28:17 +0100

    correction1

--- js/script.js
+++ js/script.js
@@ -5,26 +5,29 @@
 4- Stampo in pagina i due tiri e il risultato.
 */
 
-const random = Math.random();
-
+//Prendo l'elemento in pagina
+const resultElement = ducument.getElementById("result");
 //Genero un numero random da 1 a 6 per il giocatore
-
-const user = Math.floor(random * 6) + 1;
-console.log("result user: " + user);
-
-//Genero un numero random da 1 a 6 per il giocatore
-const cpu = Math.floor(random * 6) + 1;
-console.log("result cpu: " + cpu);
+const userNumber = Math.floor(Math.random() * 6) + 1;
+//Genero un numero random da 1 a 6 per il computer
+const cpuNumber = Math.floor(Math.random() * 6) + 1;
+console.log(userNumber, cpuNumber);
 
 //Stabilisco il vincitore, in base a chi fa il punteggio più alto
-/*if (result user > result cpu  ){
-const result
-} else if (result user < result cpu) {
+let message = "pareggio";
 
-}else{
-
+if (userNumber > cpuNumber) {
+  message = "Ha vinto il giocatore";
+} else if (userNumber < cpuNumber) {
+  message = "Ha vinto il computer";
 }
-*/
+
+console.log(message);
+resultElement.innerHTML = `
+<div>Tiro del computer: <strong>${userNumber}</strong></div>
+<div>Tiro del giocatore: <strong>${userNumber}</strong></div>'
+<h3>${message}</h3>
+`;
 
 //Stampo in pagina i due tiri e il risultato. TO DO
 /*
@@ -33,4 +36,3 @@ Chiedi all’utente la sua email, con un piccolo form.
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
-const email = [];
